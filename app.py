994c0e52c61c8f68d95ea3a5073480d49ad2a60d
Einar Nilsen-Nygaard <einarnn@cisco.com>
2015-10-13 22:57:45 +0100

    tweaks

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+import os
 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -12,4 +14,6 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
+    print(os.environ['APP_SETTINGS'])
     app.run()
+    
